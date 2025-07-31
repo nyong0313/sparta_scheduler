c3f7e09989771433b663336fd774ba6cce2091b8
nyong0313 <ghdyd6625@naver.com>
2025-07-31 17:43:25 +0900

    chore: 초기 DB 설정

--- src/main/java/org/example/scheduler/SchedulerApplication.java
+++ src/main/java/org/example/scheduler/SchedulerApplication.java
@@ -2,7 +2,9 @@ package org.example.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:secrets.properties")
 @SpringBootApplication
 public class SchedulerApplication {
 
