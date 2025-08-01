1f84f9b2eeeeb16f21825d62bbc091c512aa8887
nyong0313 <ghdyd6625@naver.com>
2025-08-01 17:55:28 +0900

    feat: lv2 일정 조회 기능 추가

--- src/main/java/org/example/scheduler/controller/ScheduleController.java
+++ src/main/java/org/example/scheduler/controller/ScheduleController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.scheduler.controller.dto.ScheduleRequestDto;
 import org.example.scheduler.service.ScheduleService;
 import org.example.scheduler.service.dto.ScheduleResponseDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +17,16 @@ public class ScheduleController {
 
     @PostMapping
     public ScheduleResponseDto saveSchedule(@RequestBody ScheduleRequestDto scheduleRequestDto) {
-
         return scheduleService.saveSchedule(scheduleRequestDto);
+    }
+
+    @GetMapping
+    public List<ScheduleResponseDto> getSchedules() {
+        return scheduleService.getSchedules();
+    }
+
+    @GetMapping("/{scheduleId}")
+    public ScheduleResponseDto getScheduleById(@PathVariable Long scheduleId) {
+        return scheduleService.getScheduleById(scheduleId);
     }
 }
--- src/main/java/org/example/scheduler/service/ScheduleService.java
+++ src/main/java/org/example/scheduler/service/ScheduleService.java
@@ -8,6 +8,9 @@ import org.example.scheduler.service.dto.ScheduleResponseDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
@@ -16,7 +19,6 @@ public class ScheduleService {
 
     @Transactional
     public ScheduleResponseDto saveSchedule(ScheduleRequestDto scheduleRequestDto) {
-
         Schedule schedule = new Schedule(
                 scheduleRequestDto.getTitle(),
                 scheduleRequestDto.getWriterName(),
@@ -32,6 +34,38 @@ public class ScheduleService {
                 savedSchedule.getContent(),
                 savedSchedule.getCreatedAt(),
                 savedSchedule.getUpdatedAt()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<ScheduleResponseDto> getSchedules() {
+        // findAll 반환값은 List
+        List<Schedule> scheduleList = scheduleRepository.findAll();
+        List<ScheduleResponseDto> scheduleResponseDtoList = new ArrayList<>();
+        for (Schedule schedule : scheduleList) {
+            scheduleResponseDtoList.add(new ScheduleResponseDto(
+                    schedule.getId(),
+                    schedule.getTitle(),
+                    schedule.getWriterName(),
+                    schedule.getContent(),
+                    schedule.getCreatedAt(),
+                    schedule.getUpdatedAt()
+            ));
+        }
+        return scheduleResponseDtoList;
+    }
+
+    @Transactional(readOnly = true)
+    public ScheduleResponseDto getScheduleById(Long id) {
+        Schedule schedule = scheduleRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("일정을 찾을 수 없습니다. id: " + id));
+        return new ScheduleResponseDto(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getWriterName(),
+                schedule.getContent(),
+                schedule.getCreatedAt(),
+                schedule.getUpdatedAt()
         );
     }
 }
