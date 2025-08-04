c07b98bfaf49c1a846387bcbdbff846489126ff7
nyong0313 <ghdyd6625@naver.com>
2025-08-04 11:52:21 +0900

    chore: JPA ddl-auto=update 변경

--- src/main/java/org/example/scheduler/controller/CommentController.java
+++ src/main/java/org/example/scheduler/controller/CommentController.java
@@ -2,11 +2,9 @@ package org.example.scheduler.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.scheduler.controller.dto.CommentRequestDto;
-import org.example.scheduler.controller.dto.ScheduleRequestDto;
 import org.example.scheduler.service.CommentService;
 import org.example.scheduler.service.dto.ApiResponse;
 import org.example.scheduler.service.dto.CommentResponseDto;
-import org.example.scheduler.service.dto.ScheduleResponseDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
