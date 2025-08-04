15aec105c5a3b7a6deaa3a1f073ec76f2a3ad23c
nyong0313 <ghdyd6625@naver.com>
2025-08-04 11:11:27 +0900

    feat: lv6 일정 단건 조회 업그레이드

--- src/main/java/org/example/scheduler/service/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/scheduler/service/dto/ScheduleResponseDto.java
@@ -5,7 +5,9 @@ import org.example.scheduler.entity.Comment;
 import org.example.scheduler.entity.Schedule;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ScheduleResponseDto {
@@ -15,6 +17,7 @@ public class ScheduleResponseDto {
     private final String content;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
+    private final List<CommentResponseDto> commentResponseDtos;
 
     public ScheduleResponseDto(
             Long id,
@@ -22,23 +25,31 @@ public class ScheduleResponseDto {
             String writerName,
             String content,
             LocalDateTime createdAt,
-            LocalDateTime updatedAt) {
+            LocalDateTime updatedAt,
+            List<CommentResponseDto> commentResponseDtos) {
         this.id = id;
         this.title = title;
         this.writerName = writerName;
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.commentResponseDtos = commentResponseDtos;
     }
 
     public static ScheduleResponseDto from(Schedule schedule) {
+        List<CommentResponseDto> commentResponseDtos = schedule.getComments()
+                .stream()
+                .map(CommentResponseDto::from)
+                .collect(Collectors.toList());
+
         return new ScheduleResponseDto(
                 schedule.getId(),
                 schedule.getTitle(),
                 schedule.getWriterName(),
                 schedule.getContent(),
                 schedule.getCreatedAt(),
-                schedule.getUpdatedAt()
+                schedule.getUpdatedAt(),
+                commentResponseDtos
         );
     }
 }
